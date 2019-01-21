93b4654cdd1b7c978e0546416f2523b361ff634f
linkel <ironcarbide@gmail.com>
2019-01-21 14:15:55 -0800

    handleAddFriend function POSTing to server added

--- friends/src/App.js
+++ friends/src/App.js
@@ -20,10 +20,32 @@ class App extends Component {
         })
       .catch(err => console.log(err));
   }
+  handleAddFriend = (event) => {
+    event.preventDefault();
+    let friendObj = { //cool, it handles new ids
+      name : event.target[0].value,
+      age : event.target[1].value,
+      email : event.target[2].value,
+    }
+    axios     
+      .post(`http://localhost:5000/friends`, friendObj)
+      .then(response => {
+      this.setState({friends: response.data})
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  }
   render() {
     return (
       <div className="App">
         <FriendsList friends={this.state.friends}/>
+        <form onSubmit={this.handleAddFriend}>
+          <input type="text" placeholder="name"></input>
+          <input type="number" placeholder="age"></input>
+          <input type="text" placeholder="email"></input>
+          <input type="submit"></input>
+        </form>
       </div>
     );
   }
--- friends/src/components/Friend.jsx
+++ friends/src/components/Friend.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FriendWrapper = styled.div`
+    padding: 20px;
+`
 
 const Friend = (props) => {
     return (
-        <div>
-            {props.friend.name}
-        </div>
+        <FriendWrapper>
+            {props.friend.name}, who is {props.friend.age} years old, can be reached at {props.friend.email}.
+        </FriendWrapper>
     )
 }
 
